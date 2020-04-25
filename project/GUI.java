53fb8fd34c830c3c4784ad3273442b6e9fdd29e1
Lighter1234 <Lighter1234@seznam.cz>
2020-04-25 20:03:59 +0200

    Test push

--- project/GUI.java
+++ project/GUI.java
@@ -1,5 +1,7 @@
 package project;
 
+import waterflowsim.Simulator;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,15 +12,37 @@ public class GUI {
 
     private static boolean simulationRunning = true;
 
-    public GUI(Panel panel){
-        this.panel = panel;
+    private int sim;
+    public GUI(int sim){
+        this.sim = sim;
     }
 
 
 
     private void makeGUI(){
-        makeStopButton();
+     //   this.panel = panel;
 
+        JFrame frame = new JFrame("A18B0307P");
+        Panel p = new Panel(sim);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //       p.setPreferredSize(new Dimension(800,600));
+        frame.add(p);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        Timer timer;
+        int timerPeriod = 1000 / 25; // Prekreslit okno 25krat za 1000 milisekund
+        long startTime = System.currentTimeMillis();
+        timer = new Timer(timerPeriod, new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Simulator.nextStep(0.1);
+                p.repaint();
+            }
+        });
+        timer.start();
 
     }
 
